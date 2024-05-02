5cb6d72448185017a0cab8c71cff104a6802bd88
AccelGuide <167211223+AccelGuide@users.noreply.github.com>
2024-05-02 13:36:24 +0200

    Projektdateien hinzufügen.

---
+++ comparison_operators_random_generator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+    class comparison_operators_random_generator
+{
+    static Random random = new Random();
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Taste drücken || ESC zum Beenden");
+
+        do
+        {
+            // Generiere zwei zufällige Zahlen
+            int value1 = random.Next(1, 100);
+            int value2 = random.Next(1, 100);
+
+            // Wählt zufällig einen Vergleichsoperator
+            int operatorChoice = random.Next(1, 7);
+            bool result;
+
+            switch (operatorChoice)
+            {
+                case 1:
+                    result = value1 > value2;
+                    Console.WriteLine($"{value1} > {value2} = {result}");
+                    break;
+                case 2:
+                    result = value1 >= value2;
+                    Console.WriteLine($"{value1} >= {value2} = {result}");
+                    break;
+                case 3:
+                    result = value1 == value2;
+                    Console.WriteLine($"{value1} == {value2} = {result}");
+                    break;
+                case 4:
+                    result = value1 < value2;
+                    Console.WriteLine($"{value1} < {value2} = {result}");
+                    break;
+                case 5:
+                    result = value1 <= value2;
+                    Console.WriteLine($"{value1} <= {value2} = {result}");
+                    break;
+                case 6:
+                    result = value1 != value2;
+                    Console.WriteLine($"{value1} != {value2} = {result}");
+                    break;
+            }
+
+            Console.WriteLine("Taste drücken || ESC zum Beenden");
+        } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+    }
+}
