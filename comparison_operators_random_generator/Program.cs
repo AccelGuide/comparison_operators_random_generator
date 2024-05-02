1ccb786b9baefb192014451eef3cafeb5a13240d
AccelGuide <167211223+AccelGuide@users.noreply.github.com>
2024-05-02 14:10:06 +0200

    #+ Comments

--- comparison_operators_random_generator/Program.cs
+++ comparison_operators_random_generator/Program.cs
@@ -16,6 +16,20 @@
 
             // Wählt zufällig einen Vergleichsoperator
             int operatorChoice = random.Next(1, 7);
+            /*
+             * Die Methode random.Next(1, 6) würde in disem Fall nicht korrekt sein,
+             * wenn zwischen sechs verschiedenen Vergleichsoperatoren ausgewählt werden solte.
+             * Wenn du also zufällig einen von sechs Operatoren wählen möchtest, sollte der Aufruf random.Next(1, 7) lauten. Dies gibt dir eine zufällige Ganzzahl von 1 bis einschließlich 6. Hier sind die Optionen:
+             *
+             *    1: größer als (>)
+             *    2: größer oder gleich (>=)
+             *    3: gleich (==)
+             *    4: kleiner als (<)
+             *    5: kleiner oder gleich (<=)
+             *    6: ungleich (!=)
+
+             *  ----> Mit random.Next(1, 7) deckt man somit alle sechs Möglichkeiten ab.
+             */
             bool result;
 
             switch (operatorChoice)
@@ -25,6 +39,17 @@
                     Console.WriteLine($"{value1} > {value2} = {result}");
                     // Console.WriteLine(String.Format("{0} > {1} = {2}", value1, value2, result));
                     // Console.WriteLine(value1 + " > " + value2 + " = " + result);
+                    /*
+                     * Erklärung:
+                     * -----
+                     * String.Format: Diese Methode nimmt eine Formatzeichenkette und eine Liste von Argumenten.
+                     * Die Platzhalter {0}, {1}, {2} usw. in der Formatzeichenkette werden durch die entsprechenden Argumente ersetzt.
+                     * In diesem Beispiel entspricht {0} value1, {1} value2 und {2} result.
+                     * -----
+                     * String-Konkatenation: Hierbei verbindest du einfach Teile der Nachricht durch das +-Zeichen.
+                     * Dies ist weniger effizient als String.Format oder String Interpolation,
+                     * besonders wenn viele Strings zusammengefügt werden, da es mehr temporäre String-Objekte erzeugt.
+                     */
                     break;
                 case 2:
                     result = value1 < value2;
